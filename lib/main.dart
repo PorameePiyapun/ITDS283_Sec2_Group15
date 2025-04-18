02d7fc798e7ecf5fdc6d2675b28cd67fa3d816d3
pengdeadeye <112195493+pengdeadeye@users.noreply.github.com>
2025-04-19 00:40:04 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ import '/screens/login/login_2.dart';
 import '/screens/login/login_3.dart';
 import 'package:help_you_to_mu_health/screens/browse.dart' as browse; // Alias for BrowseScreen
 import 'package:help_you_to_mu_health/screens/reservationScreen.dart' as reservation; // Alias for ReservationScreen
+import 'screens/setting/account.dart';
+import 'screens/setting/contactus.dart';
+import 'screens/setting/language.dart';
+import 'screens/setting/noti.dart';
+import 'screens/setting/privacy.dart';
+import 'screens/setting/setting.dart';
+
 
 void main() {
   runApp(const HelpYouToMUHealthApp());
@@ -28,6 +35,14 @@ class HelpYouToMUHealthApp extends StatelessWidget {
         '/signup': (context) => SignUpScreen(),
         '/login_2': (context) => const Login2Screen(),
         '/login_3': (context) => const Login3Screen(),
+
+        '/account': (context) =>  AccountPage(),
+        '/contactus': (context) => ContactUsPage(),
+        '/language': (context) =>  LanguagePage(),
+        '/noti': (context) =>  NotificationPage(),
+        '/privacy': (context) =>  PrivacyPage(),
+        '/setting': (context) =>  SettingsPage(),
+        
         '/browse': (context) => browse.BrowseScreen(), // Use alias for BrowseScreen
         '/reservation': (context) => reservation.ReservationScreen(), // Use alias for ReservationScreen
       },
